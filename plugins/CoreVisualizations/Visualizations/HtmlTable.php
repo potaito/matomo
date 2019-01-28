ceade7c2a9a9d84d73d30bf13d9b1ee1d7651a5d
sgiehl <stefan@matomo.org>
2019-01-28 14:04:51 +0100

    adds ui test

--- plugins/CoreVisualizations/Visualizations/HtmlTable.php
+++ plugins/CoreVisualizations/Visualizations/HtmlTable.php
@@ -158,7 +158,6 @@ class HtmlTable extends Visualization
             $this->dataTable->applyQueuedFilters();
         }
 
-
         parent::beforeGenericFiltersAreAppliedToLoadedDataTable();
     }
 
